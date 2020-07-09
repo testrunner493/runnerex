5172123eff8064442f33ced3cc2d6b2ec96b43d5
testrunner493 <68055992+testrunner493@users.noreply.github.com>
2020-07-09 21:52:22 +0900

    script path

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 const video = document.getElementById('video')
 
-Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('models/'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('models/'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('models/'),
-  faceapi.nets.faceExpressionNet.loadFromUri('models/')
-]).then(startVideo)
+// Promise.all([
+//   faceapi.nets.tinyFaceDetector.loadFromUri('models/'),
+//   faceapi.nets.faceLandmark68Net.loadFromUri('models/'),
+//   faceapi.nets.faceRecognitionNet.loadFromUri('models/'),
+//   faceapi.nets.faceExpressionNet.loadFromUri('models/')
+// ]).then(startVideo)
 
 function startVideo() {
-  navigator.getUserMedia(
-    { video: {} },
-    stream => video.srcObject = stream,
-    err => console.error(err)
-  )
+  navigator.mediaDevices.getUserMedia({video: {}}) .then((stream)=> {video.srcObject = stream;}, (err)=> console.error(err));
 }
 
 
