c02e4aabd5075327ab9cf4c3660b0a757ed4a6aa
testrunner493 <68055992+testrunner493@users.noreply.github.com>
2020-07-10 01:05:24 +0900

    Update webcam.js

--- webcam.js
+++ webcam.js
@@ -1047,10 +1047,10 @@ else {
 }
 
 
-Promise.all([ faceapi.nets.tinyFaceDetector.loadFromUri('models/face_landmark_68_model-weights_manifest.json'),
-faceapi.nets.faceLandmark68Net.loadFromUri('models/face_expression_model-weights_manifest.json'),
-faceapi.nets.faceRecognitionNet.loadFromUri('models/face_recognition_model-weights_manifest.json'),
-faceapi.nets.faceExpressionNet.loadFromUri('models/face_landmark_68_model-weights_manifest.json')]).then(console.log("face-lib loaded"))
+Promise.all([ faceapi.nets.tinyFaceDetector.loadFromUri('models/'),
+faceapi.nets.faceLandmark68Net.loadFromUri('models/'),
+faceapi.nets.faceRecognitionNet.loadFromUri('models/'),
+faceapi.nets.faceExpressionNet.loadFromUri('models/')]).then(console.log("face-lib loaded"))
 
 
 video.addEventListener('play', () => {
