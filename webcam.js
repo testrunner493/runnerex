fa700761c4ec093eef0732e3abdc8307d65ecc21
testrunner493 <68055992+testrunner493@users.noreply.github.com>
2020-07-10 00:41:56 +0900

    Update webcam.js

--- webcam.js
+++ webcam.js
@@ -1050,7 +1050,7 @@ else {
 Promise.all([ faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
 faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
 faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-faceapi.nets.faceExpressionNet.loadFromUri('/models')])
+faceapi.nets.faceExpressionNet.loadFromUri('/models')]).then(console.log("face-lib loaded"))
 
 
 video.addEventListener('play', () => {
@@ -1064,10 +1064,10 @@ video.addEventListener('play', () => {
 	  const resizedDetections = faceapi.resizeResults(detections, displaySize)
 	  canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
 	  faceapi.draw.drawDetections(canvas, resizedDetections)
-	  // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+	  faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
 	  faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
   
-	  // console.log(faceval[0]['expressions']['happy'])
+	  console.log(faceval[0]['expressions']['happy'])
   
 	  if(faceval[0]['expressions']['happy'] > 0.8 ){
   
