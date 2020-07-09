7d0ae0316f91cf428a5a67fcbba2054be940de54
testrunner493 <68055992+testrunner493@users.noreply.github.com>
2020-07-10 00:32:02 +0900

    new

--- webcam.js
+++ webcam.js
@@ -1047,6 +1047,37 @@ else {
 }
 
 
+await faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+await faceapi.nets.faceLandmark68Net.loadFromUri('/models')
+await faceapi.nets.faceRecognitionNet.loadFromUri('/models')
+await faceapi.nets.faceExpressionNet.loadFromUri('/models')
 
+
+Webcam.addEventListener('play', () => {
+	const canvas = faceapi.createCanvasFromMedia(video)
+	document.body.append(canvas)
+	const displaySize = { width: video.width, height: video.height }
+	faceapi.matchDimensions(canvas, displaySize)
+	setInterval(async () => {
+	  const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+	  const faceval = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceExpressions()
+	  const resizedDetections = faceapi.resizeResults(detections, displaySize)
+	  canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+	  faceapi.draw.drawDetections(canvas, resizedDetections)
+	  // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+	  faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+  
+	  // console.log(faceval[0]['expressions']['happy'])
+  
+	  if(faceval[0]['expressions']['happy'] > 0.8 ){
+  
+		gameInstance.SendMessage('SmileGauge', 'getVal')
+  
+	  }
+  
+  
+	}, 100)
+	
+  })
 
 }(window));
