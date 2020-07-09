6473d0960dce347300f8a9d6288b58c839a1be27
testrunner493 <68055992+testrunner493@users.noreply.github.com>
2020-07-09 21:56:34 +0900

    copy model to root

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const video = document.getElementById('video')
 
-// Promise.all([
-//   faceapi.nets.tinyFaceDetector.loadFromUri('models/'),
-//   faceapi.nets.faceLandmark68Net.loadFromUri('models/'),
-//   faceapi.nets.faceRecognitionNet.loadFromUri('models/'),
-//   faceapi.nets.faceExpressionNet.loadFromUri('models/')
-// ]).then(startVideo)
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('models/'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('models/'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('models/'),
+  faceapi.nets.faceExpressionNet.loadFromUri('models/')
+]).then(startVideo)
 
 function startVideo() {
   navigator.mediaDevices.getUserMedia({video: {}}) .then((stream)=> {video.srcObject = stream;}, (err)=> console.error(err));
